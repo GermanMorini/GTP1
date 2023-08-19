bb16f5fdd8478150758246421de338c241dcd77e
Germán Morini <germorini25@gmail.com>
2023-08-19 10:05:57 -0300

    gtp13 mejorado

--- Guia extra 2/gtp13.cpp
+++ Guia extra 2/gtp13.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-
 // Incluye el archivo de definición de funciones
 // Se usan "" para que el compilador los busque en el directorio actual, o
 // sino en los directorios especificados en la configuración
@@ -18,27 +17,40 @@ void tmp_conversion(float n) {
 }
 
 typedef void (*Operation)(float);
+const int OPERATIONS = 3;
+const Operation op[OPERATIONS] = {
+        NULL,
+        dist_conversion,
+        tmp_conversion
+};
+
 Operation parse_opt(int o) {
-        switch(o) {
-                case 1:
-                        return dist_conversion;
-                case 2:
-                        return tmp_conversion;
-                case 3:
-                        exit(0);
-                default:
-                        puts("Operación inexistente");
-                        return NULL;
-        }
+        return op[o*(o > 0 and o < OPERATIONS)];
 }
+
+// Operation parse_opt(int o) {
+//         switch(o) {
+//                 case 1:
+//                         return dist_conversion;
+//                 case 2:
+//                         return tmp_conversion;
+//                 case 3:
+//                         exit(0);
+//                 default:
+//                         puts("Operación inexistente");
+//                         return NULL;
+//         }
+// }
 
 int prompt_opt() {
         int opt;
 
         do {
-                cout << "1 – Conversión de distancia\n2 – Conversión de temperatura\n3 – Salir\n";
+                cout << "1 – Conversión de distancia\n2 – Conversión de temperatura\n3 – Salir\n\n> ";
                 cin >> opt;
-        } while(opt < 1 or opt > 3);
+
+                if(opt == 3) exit(0);
+        } while(opt < 1 or opt > OPERATIONS);
 
         return opt;
 }
@@ -49,6 +61,7 @@ float get_val() {
         do {
                 cout << "Ingrese el valor a convertir: ";
                 cin >> val;
+                cout << "\n\n";
         } while(val < 0);
 
         return val;
